144eb4f50233545535b9c79e657b1731fc9f7af7
Jason Kulak <JasonKulak@yahoo.com>
2024-03-24 21:15:53 -0500

    NFL queries 2

--- NFL.sql
+++ NFL.sql
@@ -11,13 +11,16 @@ SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
 SELECT MAX(id) FROM players;
 
 -- 5. The team names and head coaches of the NFC North and AFC East
-
+SELECT head_coach, name FROM teams WHERE conference = 'NFC' AND division = 'North';
+SELECT head_coach, name FROM teams WHERE conference = 'AFC' AND division = 'East';
 
 -- 6. The 50 players with the highest salaries
 
+--NOT CORRECT.  FIX BEFORE COMMITTING HOMEWORK##################
+?????????????SELECT salary, name FROM players LIMIT 50;?????????????
 
 -- 7. The average salary of all NFL players
-
+SELECT AVG(salary) FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
 
@@ -29,12 +32,12 @@ SELECT MAX(id) FROM players;
 
 
 -- 11. The average salary for a DE in the nfl
-
+SELECT AVG(salary) FROM players WHERE position = 'DE';
 
 -- 12. The names of all the players on the Buffalo Bills
-
+SELECT * FROM players WHERE team_id = 1;
 
 -- 13. The total salary of all players on the New York Giants
-
+SELECT SUM(salary) FROM players WHERE team_id = 18;
 
 -- 14. The player with the lowest salary on the Green Bay Packers
